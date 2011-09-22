ad01e66305044c2d3bbb6529cb5231c73a284e56
Mark Cahill <mark@tiemonster.info>
2011-09-22 12:03:32 -0400

    Maybe the compilation units need to be compiled first

--- dns/test.sh
+++ dns/test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-javac dns/NSServer.java
 javac dns/NSServerThread.java
 javac dns/ShutdownIntercept.java
+javac dns/NSServer.java
 javac dns/NSClient.java
 java dns/NSServer > log 2> /dev/null &
 echo $! > server.PID
