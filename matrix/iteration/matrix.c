84030021c5e896261946a7dfae453f11d07b78ae
Mark Cahill <mark@tiemonster.info>
2011-10-01 09:19:48 -0400

    Iteration and simulation test set up.

--- matrix/iteration/matrix.c
+++ matrix/iteration/matrix.c
@@ -1,3 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int main(int argc, char *argv[]) {
+  int size = atoi(argv[1]), rows, cols;
+  printf("%*s%*d.0\t", 15, "Iterative", 15, size);
+
+  // Generate matrices
+
+  for (rows = 0; rows < size; rows++) {
+    for (cols = 0; cols < size; cols++) {
+      // Calculate matrix multiplication
+    }
+  }
+
+  return 0;
+}
