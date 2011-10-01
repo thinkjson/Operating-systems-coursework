3478b0cdd88ea9c32e23086f5c9446f18a6be06d
Mark Cahill <mark@tiemonster.info>
2011-10-01 10:00:27 -0400

    Generated the matrices

--- matrix/iteration/matrix.c
+++ matrix/iteration/matrix.c
@@ -1,15 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main(int argc, char *argv[]) {
   int size = atoi(argv[1]), rows, cols;
+  int matrix1[size][size], matrix2[size][size], result[size][size];
   printf("%*s%*d.0\t", 15, "Iterative", 15, size);
 
   // Generate matrices
+  srand(time(NULL));
 
   for (rows = 0; rows < size; rows++) {
     for (cols = 0; cols < size; cols++) {
-      // Calculate matrix multiplication
+      matrix1[rows][cols] = rand();
+      matrix2[rows][cols] = rand();
+      sleep(0);
+    }
+  }
+
+  // Do the matrix multiplication
+  for (rows = 0; rows < size; rows++) {
+    for (cols = 0; cols < size; cols++) {
+      result[rows][cols] = 0;
     }
   }
 
