ccb24ceaa16f28b953b2c2ab780e3dbcf8a32630
Mark Cahill <mark@tiemonster.info>
2011-10-01 13:13:05 -0400

    Working iterative implementation

--- matrix/iteration/matrix.c
+++ matrix/iteration/matrix.c
@@ -3,7 +3,7 @@
 #include <time.h>
 
 int main(int argc, char *argv[]) {
-  int size = atoi(argv[1]), rows, cols;
+  int size = atoi(argv[1]), rows, cols, z;
   int matrix1[size][size], matrix2[size][size], result[size][size];
   printf("%*s%*d.0\t", 15, "Iterative", 15, size);
 
@@ -14,6 +14,7 @@ int main(int argc, char *argv[]) {
     for (cols = 0; cols < size; cols++) {
       matrix1[rows][cols] = rand();
       matrix2[rows][cols] = rand();
+      result[rows][cols] = 0;
       sleep(0);
     }
   }
@@ -21,7 +22,9 @@ int main(int argc, char *argv[]) {
   // Do the matrix multiplication
   for (rows = 0; rows < size; rows++) {
     for (cols = 0; cols < size; cols++) {
-      result[rows][cols] = 0;
+      for (z = 0; z < size; z++) {
+        result[rows][z] += matrix1[rows][cols] * matrix2[cols][z];
+      }
     }
   }
 
