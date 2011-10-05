5dd8dfb4d2e0e049c1b125cf976996fdd118b546
Mark Cahill <mark@tiemonster.info>
2011-10-05 19:45:15 -0400

    Finished java threads implementation

--- matrix/java-threads/src/Matrix.java
+++ matrix/java-threads/src/Matrix.java
@@ -6,7 +6,8 @@ public class Matrix {
 	private int size;
 	private int nprocs;
 	private Random prng;
-	private int[][] matrix1, matrix2, result;
+	private int[][] matrix1, matrix2;
+	private long[][] result;
 	private MatrixMultiplier[] threads;
 	private final ArrayBlockingQueue<Integer> threadQueue;
 	
@@ -15,35 +16,61 @@ public class Matrix {
 		this.prng = new Random();
 		this.threadQueue = new ArrayBlockingQueue<Integer>(this.size * this.size);
 		this.populate();
-		this.init_threads();
 		this.fillQueue();
+		this.init_threads();
+		
+		System.out.printf("%15s%15d.0\t", "java threads", this.size);
 	}
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		@SuppressWarnings("unused")
 		Matrix matrix = new Matrix(args[0]);
 	}
 
 	private void populate() {
 		this.matrix1 = new int[this.size][this.size];
 		this.matrix2 = new int[this.size][this.size];
-		this.result = new int[this.size][this.size];
+		this.result = new long[this.size][this.size];
 		
 		// Populate matrices
 		for (int row = 0; row < this.size; row++) {
 			for (int col = 0; col < this.size; col++) {
-				this.matrix1[row][col] = this.prng.nextInt();
-				this.matrix2[row][col] = this.prng.nextInt();
+				this.matrix1[row][col] = this.prng.nextInt(Integer.MAX_VALUE);
+				this.matrix2[row][col] = this.prng.nextInt(Integer.MAX_VALUE);
 				this.result[row][col] = 0;
 			}
 		}
 	}
 	
 	private void fillQueue() {
+		// Fill queue with indices
+		for (int i = 0; i < this.size; i++) {
+			try {
+				this.threadQueue.put(i);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 		
+		// Send shutdown signal
+		for (int i = 0; i < nprocs; i++) {
+			try {
+				this.threadQueue.put(-1);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public int take() {
+		try {
+			return this.threadQueue.take();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+			return 0;
+		}
 	}
 	
 	private void init_threads() {
@@ -51,7 +78,8 @@ public class Matrix {
 		this.threads = new MatrixMultiplier[this.nprocs];
 		
 		for (int i = 0; i < nprocs; i++) {
-			this.threads[i] = new MatrixMultiplier();
+			this.threads[i] = new MatrixMultiplier(this);
+			this.threads[i].start();
 		}
 	}
 
@@ -72,7 +100,21 @@ public class Matrix {
 	/**
 	 * @param result the result to set
 	 */
-	public void setResultRow(int row, int[] resultRow) {
+	public void setResultRow(int row, long[] resultRow) {
 		this.result[row] = resultRow;
+	}
+
+	/**
+	 * @return the matrix size
+	 */
+	public int getSize() {
+		return size;
+	}
+	
+	/**
+	 * @return the queue size
+	 */
+	public int getQueueSize() {
+		return this.threadQueue.size();
 	}
 }
--- matrix/java-threads/src/MatrixMultiplier.java
+++ matrix/java-threads/src/MatrixMultiplier.java
@@ -1,10 +1,26 @@
 
-public class MatrixMultiplier implements Runnable {
+public class MatrixMultiplier extends Thread {
+	
+	Matrix matrix;
+	int size;
 
-	@Override
-	public void run() {
-		// TODO Auto-generated method stub
+	public MatrixMultiplier(Matrix matrix) {
+		this.matrix = matrix;
+		this.size = this.matrix.getSize();
 		
+		int row, cols, z;
+		long[] result;
+		while (this.matrix.getQueueSize() > 0) {
+			row = this.matrix.take();
+			result = new long[this.size];
+		    for (cols = 0; cols < this.size; cols++) {
+		        for (z = 0; z < this.size; z++) {
+		            result[z] += (long) this.matrix.getMatrix1()[row][cols] * 
+		            	(long) this.matrix.getMatrix2()[cols][z];
+		        }
+		    }
+		    
+		    this.matrix.setResultRow(row, result);
+		}
 	}
-
 }
